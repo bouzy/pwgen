87a17841ca19b8e2d2e7fbaee9b5d992c2ad9f9a
“bouzy” <bouzy@users.noreply.github.com>
2022-01-05 11:17:49 -0500

    refactoring

--- pwgen.py
+++ pwgen.py
@@ -9,13 +9,10 @@ string_choice = letters + special_characters + numbers
 passwd = []
 
 def passwd_generator():
-
     random_loop = 0
-
     while random_loop < 21:
         passwd.append(random.choice(string_choice))
         random_loop += 1
-    
     print(''.join(passwd))
 
 if __name__ == '__main__':
