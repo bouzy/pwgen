02b8a4e01b60eb5af9bcc4b4a75b497131aa3bbd
bouzy <bouzy@users.noreply.github.com>
2024-01-21 14:40:50 -0500

    update

--- pwgen.py
+++ pwgen.py
@@ -3,19 +3,52 @@
 import string
 import random
 
-special_characters = list('~`!@#$%^&*()_-+={}[]|<>?:;,.')
+digits = list(string.digits)
+special_characters = list(string.punctuation)
 lowercase_letters = list(string.ascii_lowercase)
 uppercase_letters = list(string.ascii_uppercase)
-digits = list(string.digits)
-string_choice = lowercase_letters + uppercase_letters + digits + special_characters
+string_choice = digits + special_characters + lowercase_letters + uppercase_letters
+
+passwd = []
 
 def passwd_generator():
-    passwd = []
     random_loop = 0
     while random_loop < 21:
         passwd.append(random.choice(string_choice))
         random_loop += 1
-    print(''.join(passwd))
+    validation()
+
+def validation():
+    digit = 'no'
+    special_character = 'no'
+    lowercase = 'no'
+    uppercase = 'no'
+    exitcode = 'FAILED'
+    
+    for p in passwd:
+        if p in digits and digit == 'no':
+            #print(p + ' is a digit')
+            digit = 'yes'
+        elif p in special_characters and special_character == 'no':
+            #print(p + ' is a special character')
+            special_character = 'yes'
+        elif p in lowercase_letters and lowercase == 'no':
+            #print(p + ' is a lowercase character')
+            lowercase = 'yes'
+        elif p in uppercase_letters and uppercase == 'no':
+            #print(p + ' is an uppercase character')
+            uppercase = 'yes'
+
+        if special_character == 'yes' and lowercase == 'yes' and uppercase == 'yes' and digit == 'yes':
+            exitcode = 'PASSED'
+        else:
+            exitcode = 'FAILED'
+    
+    if exitcode == 'PASSED':
+        print(''.join(passwd))
+        return
+    else:
+        passwd_generator()
 
 if __name__ == '__main__':
-    passwd_generator()
+    validation()
